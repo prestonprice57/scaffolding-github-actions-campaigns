a1c016ab0320200206db431f45ed8d19ead6c733
prestonprice57 <preston.price@resourcely.io>
2025-03-06 09:58:00 -0800

    changes

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket   = "resourcely-campaigns-terraform-state"
+    bucket   = "resourcely-campaigns-terraform-state2"
     region   = "us-west-2"
     key      = "terraform.tfstate"
   }
