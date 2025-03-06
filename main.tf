d4edcb6eb1381a82ce20ee418e96aa3bd5b69c7b
prestonprice57 <preston.price@resourcely.io>
2025-03-06 15:12:46 -0800

    Update versioning_configuration status in main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
 # main.tf
-# foo
-resource "aws_s3_bucket" "campaigns-resource_ckaMshbmm94dxCHA" {
-  bucket = "resourcely-campaigns-test-bucketquvdoihuevehxhwr"
+
+resource "aws_s3_bucket" "resourcely-campaigns-test-bucket_aannxfsuzheykxhtMshbmm94dxCHA" {
+  bucket = "resourcely-campaigns-test-bucketaannxfsuzheykxht"
 }
 
-resource "aws_s3_bucket_public_access_block" "campaigns-resource_ckaMshbmm94dxCHA" {
-  bucket                  = aws_s3_bucket.campaigns-resource_ckaMshbmm94dxCHA.id
+resource "aws_s3_bucket_public_access_block" "resourcely-campaigns-test-bucket_aannxfsuzheykxhtMshbmm94dxCHA" {
+  bucket                  = aws_s3_bucket.resourcely-campaigns-test-bucket_aannxfsuzheykxhtMshbmm94dxCHA.id
   block_public_acls       = false
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_ownership_controls" "campaigns-resource_ckaMshbmm94dxCHA" {
-  bucket = aws_s3_bucket.campaigns-resource_ckaMshbmm94dxCHA.id
+resource "aws_s3_bucket_ownership_controls" "resourcely-campaigns-test-bucket_aannxfsuzheykxhtMshbmm94dxCHA" {
+  bucket = aws_s3_bucket.resourcely-campaigns-test-bucket_aannxfsuzheykxhtMshbmm94dxCHA.id
 
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
 }
 
-resource "aws_s3_bucket_versioning" "campaigns-resource_ckaMshbmm94dxCHA" {
-  bucket = aws_s3_bucket.campaigns-resource_ckaMshbmm94dxCHA.id
+resource "aws_s3_bucket_versioning" "resourcely-campaigns-test-bucket_aannxfsuzheykxhtMshbmm94dxCHA" {
+  bucket = aws_s3_bucket.resourcely-campaigns-test-bucket_aannxfsuzheykxhtMshbmm94dxCHA.id
 
   versioning_configuration {
     status = "Enabled"
